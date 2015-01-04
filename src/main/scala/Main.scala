2f21d9816f86b0892659fd3cecb5e92756f9a781
Jakub Lewiński <jakub.lewinski@student.put.poznan.pl>
2015-01-04 19:37:57 +0100

    zmiana z shared na int, oraz banalny send

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,22 @@
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.actor.Status.Success
+import akka.actor.ActorRef
+
 object Main extends App {
   println("Hello world")
+
+  //pierwsze bajery
+
+  // Create actor system.
+  val system = ActorSystem("ItsTheFinalCountdown")
+
+  // Create new actor reference (proxy).
+  val server1 = system.actorOf(Props(new Server(Map("c"->1))), "Server1")
+  val client1 = system.actorOf(Props(new Client()), "Client1")
+
+  server1 ! 10
+
+
 }
--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,6 +1,6 @@
 import akka.actor.Actor
 
-class Server(val objects: Map[String, Shared[Integer]]) extends Actor {
+class Server(val objects: Map[String, Integer]) extends Actor {
   def receive = {
     case _ => println("received a message")
   }
