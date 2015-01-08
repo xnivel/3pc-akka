e321239512ad7f50bab7809ffff577d195b6733f
Jakub Szwachła <meszkax@gmail.com>
2015-01-08 23:49:07 +0100

    Add second server to test code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -24,6 +24,9 @@ object Main extends App {
   val map=Map("c"->((new Shared[Integer](2,0)),false));
   val server1 = system.actorOf(Props(new Server(map)), "Server1")
 
+  val map2=Map("d"->((new Shared[Integer](1,0)),false));
+  val server2 = system.actorOf(Props(new Server(map2)), "Server2")
+
   val future = server1 ? new Read("c")
   val result = Await.result(future, timeout.duration).asInstanceOf[Shared[Integer]]
   println(""+result.value)
@@ -40,11 +43,16 @@ object Main extends App {
 
   val coordinator = system.actorOf(Props[Coordinator])
   val v = Proxy(server1.path.toString, "c")
+  val u = Proxy(server2.path.toString, "d")
   val txBlock = () => transaction(system, coordinator) { tx =>
     val x = tx.read(v)
+    val y = tx.read(u)
     println(x)
+    println(y)
     tx.write(v, 5)
     println("wrote 5")
+    tx.write(u, 6)
+    println("wrote 6")
   }
   Future { txBlock() }
   Future { txBlock() }
