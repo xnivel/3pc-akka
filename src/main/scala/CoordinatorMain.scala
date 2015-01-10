41d700887070aef99f88a5996765a2bee92d6425
Jakub Szwachła <meszkax@gmail.com>
2015-01-10 19:16:41 +0100

    Get hostname from command line argument

--- src/main/scala/CoordinatorMain.scala
+++ src/main/scala/CoordinatorMain.scala
@@ -2,10 +2,16 @@ import akka.actor.{Props, ActorSystem}
 import com.typesafe.config.ConfigFactory
 
 object CoordinatorMain extends App {
-  val port = args(0)
+  val hostname = args(0)
+  val port = args(1)
 
   val config = ConfigFactory.
-    parseString(s"akka.remote.netty.tcp.port = $port").
+    parseString(
+      s"""
+         akka.remote.netty.tcp {
+           hostname = $hostname
+           port = $port
+         }""").
     withFallback(ConfigFactory.load()).resolve()
   val system = ActorSystem("CoordinatorSystem", config)
   system.actorOf(Props[Coordinator], "coordinator")
--- src/main/scala/ServerMain.scala
+++ src/main/scala/ServerMain.scala
@@ -3,10 +3,16 @@ import com.typesafe.config.ConfigFactory
 
 object ServerMain extends App {
   val name = args(0)
-  val port = args(1)
+  val hostname = args(1)
+  val port = args(2)
 
   val config = ConfigFactory.
-    parseString(s"akka.remote.netty.tcp.port = $port").
+    parseString(
+      s"""
+         akka.remote.netty.tcp {
+           hostname = $hostname
+           port = $port
+         }""").
     withFallback(ConfigFactory.load())
   val system = ActorSystem("ServerSystem", config)
   system.actorOf(Props[Server], name)
