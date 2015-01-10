de4cd6fd4d71f157fee56e472643868c93585130
Jakub Szwachła <meszkax@gmail.com>
2015-01-10 20:45:33 +0100

    Clean up Server (minor stuff)

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,19 +1,19 @@
-import akka.actor.{Props, Actor, ReceiveTimeout}
-import scala.concurrent.duration._
+import akka.actor.{Props, Actor}
 
 class Server extends Actor {
-  var objects = Map[String, (Shared[Int],Boolean)]().withDefaultValue((new Shared[Int](0,0),false))
-  val name= context.self.path.toString
+  var objects = Map[String, (Shared[Int], Boolean)]().withDefaultValue((new Shared[Int](0, 0), false))
+  val name = context.self.path.toString
+
   def receive = {
     case (name: String) => {
       println("received a message "+name)
     }
     case (msg: Read) => {
-      sender ! (objects(msg.id)._1)
+      sender ! objects(msg.id)._1
     }
     case (msg: WriteCommit) => {
       println("received a WriteCommit "+self.path.name)
-      def WriteChanges: (Map[String, (Shared[Int], Boolean)], (VarRef, Shared[Int])) => Map[String, (Shared[Int], Boolean)] = {
+      def writeChanges: (Map[String, (Shared[Int], Boolean)], (VarRef, Shared[Int])) => Map[String, (Shared[Int], Boolean)] = {
         (result: Map[String, (Shared[Int], Boolean)], elem: (VarRef, Shared[Int])) => {
           val idOfVariable = elem._1.variableId
           val nameOfServerVariable = elem._1.serverId
@@ -23,12 +23,12 @@ class Server extends Actor {
             result
         }
       }
-      objects = msg.objects.foldLeft(objects)(WriteChanges)
+      objects = msg.objects.foldLeft(objects)(writeChanges)
       println("end a WriteCommit "+self.path.name)
     }
     case (msg: AbortWithList) => {
       println("received a AbortWithList")
-      def AbortCommit: (Map[String, (Shared[Int], Boolean)], (VarRef, Shared[Int])) => Map[String, (Shared[Int], Boolean)] = {
+      def abortCommit: (Map[String, (Shared[Int], Boolean)], (VarRef, Shared[Int])) => Map[String, (Shared[Int], Boolean)] = {
         (result: Map[String, (Shared[Int], Boolean)], elem: (VarRef, Shared[Int])) => {
           val idOfVariable = elem._1.variableId
           val nameOfServerVariable = elem._1.serverId
@@ -38,7 +38,7 @@ class Server extends Actor {
             result
         }
       }
-      objects = msg.objects.foldLeft(objects)(AbortCommit)
+      objects = msg.objects.foldLeft(objects)(abortCommit)
     }
     case (msg: CanCommit) => {
       def conflicts: ((VarRef, Shared[Int])) => Boolean = {
@@ -53,24 +53,23 @@ class Server extends Actor {
         }
       }
       val needToAbort = msg.objects.exists(conflicts)
-      if(!needToAbort){
-        objects = msg.objects.foldLeft(objects)((result: Map[String, (Shared[Int],Boolean)],elem:(VarRef,Shared[Int]))=>{
+      if (!needToAbort) {
+        objects = msg.objects.foldLeft(objects)((result: Map[String, (Shared[Int],  Boolean)], elem:(VarRef, Shared[Int])) => {
           val idOfVariable=elem._1.variableId
           val nameOfServerVariable = elem._1.serverId
           if (name == nameOfServerVariable)
-            result.updated(idOfVariable,(result(idOfVariable)._1,true))
+            result.updated(idOfVariable, (result(idOfVariable)._1, true))
           else
             result
         })
         println("yes z serwera "+self.path.name)
-        val child = context.actorOf(Props(new ServerChild(msg.objects,context.self)))
+        val child = context.actorOf(Props(new ServerChild(msg.objects, context.self)))
         child forward msg
-      }else{
+      } else {
         println("no z serwera "+self.path.name)
         sender ! No()
       }
     }
-
     case _ => println("received a messagae")
   }
 }
