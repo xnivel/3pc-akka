3fdc0ec6f27a1bae4613df63b885f7d30cccfb81
Jakub Szwachła <meszkax@gmail.com>
2015-01-10 20:15:37 +0100

    Clean up messages

--- src/main/scala/messages.scala
+++ src/main/scala/messages.scala
@@ -4,12 +4,11 @@ case class Write(id: String,newVal: Int) extends Message
 case class CommitRequest(objects: Map[VarRef, Shared[Int]]) extends Message
 case class Commit() extends Message
 case class Abort() extends Message
-case class AbortWithList(objects: Set[(VarRef,Shared[Int])]) extends Message
-case class CanCommit(objects: Set[(VarRef,Shared[Int])])
+case class AbortWithList(objects: Set[(VarRef, Shared[Int])]) extends Message
+case class CanCommit(objects: Set[(VarRef, Shared[Int])]) extends Message
 case class PreCommit() extends Message
 case class DoCommit() extends Message
 case class Yes() extends Message
 case class No() extends Message
 case class Ack() extends Message
-
-case class WriteCommit(objects: Set[(VarRef,Shared[Int])])
+case class WriteCommit(objects: Set[(VarRef, Shared[Int])]) extends Message
