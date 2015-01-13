984536b9c3c06b3fdae366ab8f52f99ad5211d91
Jakub Szwachła <meszkax@gmail.com>
2015-01-13 17:34:46 +0100

    Make ClientMain hosts/ports configurable

--- src/main/scala/ClientMain.scala
+++ src/main/scala/ClientMain.scala
@@ -8,10 +8,17 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object ClientMain extends App {
   implicit val timeout = Timeout(5 seconds)
 
+  val coordinatorHost = args(0)
+  val coordinatorPort = args(1)
+  val server1Host = args(2)
+  val server1Port = args(3)
+  val server2Host = args(4)
+  val server2Port = args(5)
+
   val system = ActorSystem("Main")
-  val coordinator = system.actorSelection("akka.tcp://CoordinatorSystem@127.0.0.1:9000/user/coordinator")
-  val server1Path = "akka.tcp://ServerSystem@127.0.0.1:9001/user/server1"
-  val server2Path = "akka.tcp://ServerSystem@127.0.0.1:9002/user/server2"
+  val coordinator = system.actorSelection(s"akka.tcp://CoordinatorSystem@$coordinatorHost:$coordinatorPort/user/coordinator")
+  val server1Path = s"akka.tcp://ServerSystem@$server1Host:$server1Port/user/server1"
+  val server2Path = s"akka.tcp://ServerSystem@$server2Host:$server2Port/user/server2"
 
   val v = VarRef(server1Path, "c")
   val u = VarRef(server2Path, "d")
